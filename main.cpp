608b24e6a46fb1e19aed5d374bebbf2eefcea5cb
shawnjoseph <shawnjoseph@mail.usf.edu>
2016-06-15 13:19:10 -0400

    command processor works?

--- main.cpp
+++ main.cpp
@@ -1,30 +1,31 @@
 #include <iostream>
-#include "DynStack.h"
+#include "CommandProcessor.h"
 
 using namespace std;
 
 int main() {
-    DynStack<int> * stack1 = new DynStack<int>();
-    DynStack<int> * stack2 = new DynStack<int>(12);
-    stack2->push(1);
-    stack2->push(10);
-    stack2->push(100);
-    stack2->push(1);
-    stack2->push(100);
-    stack2->push(10);
-    stack2->push(1);
-    stack2->push(100);
-    stack2->push(999);
-    stack2->push(99);
+//    DynStack<int> * stack1 = new DynStack<int>();
+//    DynStack<int> * stack2 = new DynStack<int>(12);
+//    stack2->push(1);
+//    stack2->push(10);
+//    stack2->push(100);
+//    stack2->push(1);
+//    stack2->push(100);
+//    stack2->push(10);
+//    stack2->push(1);
+//    stack2->push(100);
+//    stack2->push(999);
+//    stack2->push(99);
+////    stack2->push(9);
+//    stack2->push(999);
+//    stack2->push(99);
 //    stack2->push(9);
-    stack2->push(999);
-    stack2->push(99);
-    stack2->push(9);
-//    stack2->pop();
-//    stack2->Display();
-//    stack2->erase(1);
-    stack2->display();
-//    stack1->isEmpty() == true ? cout << "Yes" << endl : cout << "No" << endl;
-//    cout << "Hello, World!" << endl;
+////    stack2->pop();
+////    stack2->Display();
+////    stack2->erase(1);
+//    stack2->display();
+////    stack1->isEmpty() == true ? cout << "Yes" << endl : cout << "No" << endl;
+////    cout << "Hello, World!" << endl;
+    CommandProcessor::Process_Commands();
     return 0;
 }
